336337bcceaa8a991c84d9d212213c8e8eef4a75
Lala <edwinrmrjr@gmail.com>
2024-06-13 16:32:19 +0800

    image viewer

--- images.py
+++ images.py
@@ -1,19 +1,62 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
-img = Image.open('logo.jpg')
-
-img.save('logo.ico')
-
 root = Tk()
 root.title("lets learn")
-root.iconbitmap('logo.ico')
+root.iconbitmap('images/logo.ico')
 
-my_img = ImageTk.PhotoImage(Image.open("logo.jpg"))
-my_label = Label(image=my_img)
-my_label.pack()
+my_img1 = ImageTk.PhotoImage(Image.open("images/logo.ico"))
+my_img2 = ImageTk.PhotoImage(Image.open("images/c#logo.jpg"))
+my_img3 = ImageTk.PhotoImage(Image.open("images/jslogo.jpg"))
+my_img4 = ImageTk.PhotoImage(Image.open("images/javalogo.jpg"))
+my_img5 = ImageTk.PhotoImage(Image.open("images/clogo.jpg"))
 
-button_quit = Button(root, text="Exit Program", command=root.quit)
-button_quit.pack()
+image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
+
+my_label = Label(image=my_img1)
+my_label.grid(row=0, column=0, columnspan=3)
+
+def forward(image_number):
+    global my_label
+    global button_forward
+    global button_back
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+
+    if image_number == 5:
+        button_forward = Button(root, text=">>", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
+
+def back(image_number):
+    global my_label
+    global button_forward
+    global button_back
+
+    my_label.grid_forget()
+    my_label = Label(image=image_list[image_number-1])
+    button_forward = Button(root, text=">>", command=lambda: forward(image_number+1))
+    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
+
+    if image_number == 1:
+        button_back = Button(root, text="<<", state=DISABLED)
+
+    my_label.grid(row=0, column=0, columnspan=3)
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2)
+
+
+button_back = Button(root, text="<<", command=DISABLED)
+button_exit = Button(root, text="Exit Program", command=root.quit)
+button_forward = Button(root, text=">>", command=lambda: forward(2))
+
+button_back.grid(row=1, column=0)
+button_exit.grid(row=1, column=1)
+button_forward.grid(row=1, column=2)
 
 root.mainloop()
