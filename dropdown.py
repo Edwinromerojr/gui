523d8dd296831582296c25f50cc4d8b191af7a71
Lala <edwinrmrjr@gmail.com>
2024-06-18 16:54:52 +0800

    Dropdown Menus

---
+++ dropdown.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from PIL import Image, ImageTk
+
+root = Tk()
+root.title("lets learn")
+root.iconbitmap('images/logo.ico')
+root.geometry("400x400")
+
+def show():
+    myLabel = Label(root, text=clicked.get()).pack()
+
+options = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+]
+
+
+clicked = StringVar()
+clicked.set(options[0])
+
+drop = OptionMenu(root, clicked, *options)
+drop.pack()
+
+myButton = Button(root, text="Show Selection", command=show).pack()
+
+root.mainloop()
